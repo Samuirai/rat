a070f88276b1ac4d4e31b61297e5baa17a9ad540
samuirai <git@smrrd.de>
2015-08-05 16:38:06 +0200

    stop spam of led status

--- light_control.py
+++ light_control.py
@@ -17,20 +17,28 @@ wiringpi.pinMode(18,2)
 
 rat.post_log("Light Control Started")
 
+last_status = None
+
 while True:
     settings = rat.get_settings()
     # only turn LEDs on if its getting dark, and recording enabled
     if settings:
         if sun.is_it_dark() and GPIO.input(24)==0 and int(settings['disable_recording'])==0:
             ir_led = float(settings['ir_led'])/100.0
-            rat.post_log("It's night. Set LEDs to: {0}%%".format(ir_led))
+            if last_status!='night':
+                rat.post_log("It's night. Set LEDs to: {0}%%".format(ir_led))
+                last_status = 'night'
             wiringpi.pwmWrite(18, int(1024*ir_led))
         elif GPIO.input(24)==0 and int(settings['disable_recording'])==0:
             ir_led = float(settings['ir_led_day'])/100.0
-            rat.post_log("It's day. Set LEDs to: {0}%%".format(ir_led))
+            if last_status!='day':
+                rat.post_log("It's day. Set LEDs to: {0}%%".format(ir_led))
+                last_status = 'day'
             wiringpi.pwmWrite(18, int(1024*ir_led))
         else:
-            rat.post_log("LEDs disabled")
+            if last_status!='disabled':
+                rat.post_log("LEDs and camera disabled")
+                last_status = 'disabled'
             wiringpi.pwmWrite(18, 0)
 
     time.sleep(30.0)
